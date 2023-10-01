170f6b5b4850e3a0790f8cb3c0447814dd21e411
malek <malzovich@gmail.com>
2023-10-01 19:02:24 +0100

    feat: Course Edit page UI

---
+++ components/icon-badge.tsx
@@ -0,0 +1,52 @@
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import { LucideIcon } from "lucide-react";
+
+const backgroudVariants = cva("rounded-full flex items-center justify-center", {
+  variants: {
+    variant: {
+      default: "bg-sky-100",
+      success: "bg-emeral-100",
+    },
+    size: {
+      default: "p-2",
+      sm: "p-1",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+const iconVariants = cva("", {
+  variants: {
+    variant: {
+      default: "text-sky-700",
+      success: "text-emerald-700",
+    },
+    size: {
+      default: "h-8 w-8",
+      sm: "h-4 w-4",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type BackgroundVairantsProps = VariantProps<typeof backgroudVariants>;
+type IconVairantsProps = VariantProps<typeof iconVariants>;
+
+interface IconBadgeProps extends BackgroundVairantsProps, IconVairantsProps {
+  icon: LucideIcon;
+}
+
+export const IconBadge = ({ icon: Icon, variant, size }: IconBadgeProps) => {
+  return (
+    <div className={cn(backgroudVariants({ variant, size }))}>
+      <Icon className={cn(iconVariants({ variant, size }))} />
+    </div>
+  );
+};
