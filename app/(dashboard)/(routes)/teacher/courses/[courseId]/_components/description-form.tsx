c7b347abbdc7bfc473aa5a5d5d0adaa7c34049ad
malek <malzovich@gmail.com>
2023-10-02 13:11:18 +0100

    feat: UploadThing & Image Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -8,6 +8,7 @@ import { Pencil } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import { Course } from "@prisma/client";
 
 import {
   Form,
@@ -21,9 +22,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 interface DescriptionFormProps {
-  initialData: {
-    description: string;
-  };
+  initialData: Course;
   courseId: string;
 }
 
@@ -44,7 +43,9 @@ export const DescriptionForm = ({
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData,
+    defaultValues: {
+      description: initialData?.description || "",
+    },
   });
 
   const { isSubmitting, isValid } = form.formState;
