91e5fdbd88fc3dc0ffb962cafacff6ff21efe44d
malek <malzovich@gmail.com>
2023-10-03 12:09:32 +0100

    feat: Chapter Description Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,10 +1,12 @@
-import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
-import Link from "next/link";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { ArrowLeft, LayoutDashboard } from "lucide-react";
+
+import { IconBadge } from "@/components/icon-badge";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
   params,
@@ -64,6 +66,11 @@ const ChapterIdPage = async ({
               <h2 className="text-xl">Customize your chapter</h2>
             </div>
             <ChapterTitleForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+            <ChapterDescriptionForm
               initialData={chapter}
               courseId={params.courseId}
               chapterId={params.chapterId}
